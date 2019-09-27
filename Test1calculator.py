69344e8641122617fa1ad52c0a6cca78078219c3
Honeylittlewolf <dezsinon666@gmail.com>
2019-09-27 10:43:57 +0300

    Update Test1calculator.py

--- Test1calculator.py
+++ Test1calculator.py
@@ -1,10 +1,12 @@
 #Little wolf calculator v0.1
 from colorama import init
-from colorama import Fore, Back, Style #Модуль цвета 
+from colorama import Fore, Back, Style #Модуль цвета
 init()
+print( Back.BLACK )
+print( Fore.RED )
+print("Little wolf calculator v0.1")
 print( Back.GREEN )
 print( Fore.BLACK )
-print("Little wolf calculator v0.1")
 what = input("Необходимая опирация? (+,-,*,/): ")
 print( Back.CYAN )
 print( Fore.BLACK )
@@ -14,7 +16,7 @@ b = float(input("Второе число: "))
 print( Back.YELLOW )
 print( Fore.BLACK )
 if what == "+":
-	c = a + b 
+	c = a + b
 	print("Результат: " + str(c))
 
 elif what == "-":
@@ -22,7 +24,7 @@ elif what == "-":
 	print("Результат: " + str(c))
 
 elif what == "*":
-    c = a * b	
+    c = a * b
     print("Результат: " + str(c))
 
 elif what == "/":
